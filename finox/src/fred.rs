501596f5f63d1138b5c3a65d8ffe15ab2a3ff842
anandijain <anandj@uchicago.edu>
2020-05-23 22:56:26 -0500

    fred obs

--- finox/src/fred.rs
+++ finox/src/fred.rs
@@ -10,7 +10,7 @@ extern crate url;
 use futures::stream::StreamExt;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::{thread, time};
+use std::{thread, time, fs::{File}, path::Path, io::{prelude::*, BufReader}};
 use url::Url;
 
 //use std::sync::{Arc, Mutex};
@@ -22,7 +22,7 @@ pub const ONE_SEC: time::Duration = time::Duration::from_secs(1);
 
 fn extract_id(input: &str) -> Option<&str> {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"id=(?P<id>\d*)").unwrap();
+        static ref RE: Regex = Regex::new(r"id=(?P<id>\w*)&").unwrap();
     }
     RE.captures(input)
         .and_then(|cap| cap.name("id").map(|login| login.as_str()))
@@ -32,9 +32,11 @@ fn extract_id(input: &str) -> Option<&str> {
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let path = "fred/category/series";
-    let q = "category_id=";
+    let path = "fred/series/observations";
+    let q = "series_id=";
     //println!("{:?}", url.path());
-    let urls = gen_queries(path, q);
+    let ids = read_tickers("../data/fred/fred_series_ids.txt");
+    let urls = gen_queries(ids, path, q);
     println!("{:#?}", urls);
     let roots = lil_fetch(urls.iter().map(|x| x.to_string()).collect::<Vec<_>>()).await;
     println!("{:#?}", roots);
@@ -45,16 +47,16 @@ async fn main() -> Result<(), reqwest::Error> {
 pub async fn lil_fetch(urls: Vec<String>) -> Vec<String> {
     let fetches = futures::stream::iter(urls.into_iter().map(|url| async move {
         if let Ok(res) = reqwest::get(&url).await {
-            if let Ok(root) = res.json::<Root>().await {
+            if let Ok(root) = res.json::<SeriesObsRoot>().await {
                 println!("url: {}, {:#?}", url.clone(), root);
                 thread::sleep(ONE_SEC);
                 let recs = root.to_recs();
                 let id = extract_id(&url).unwrap();
-                let fp = format!("{}{}_category.csv", WRITE_PATH, id);
+                let fp = format!("{}series/{}_series.csv", WRITE_PATH, id);
                 if let Ok(_) = write_csv(
                     &fp,
                     recs,
-                    SERIES_HEADER
+                    OBS_HEADER
                         .iter()
                         .map(|x| x.to_string())
                         .collect::<Vec<_>>(),
@@ -89,13 +91,14 @@ pub fn write_csv(fp: &str, recs: Vec<Vec<String>>, header: Vec<String>) -> Resul
     wtr.flush()?;
     Ok(())
 }
-fn gen_queries(path: &str, q: &str) -> Vec<Url> {
+
+fn gen_queries(ids: Vec<String>, path: &str, q: &str) -> Vec<Url> {
     let mut urls: Vec<Url> = vec![];
     let root = Url::parse("https://api.stlouisfed.org");
-    for i in 1..=250 {
+    for i in ids.iter() {
         if let Ok(r) = root.clone() {
             let q = format!(
-                "{}?{}{}&api_key={}&file_type=json",
+                "{}?{}{}&api_key={}&file_type=json&limit=10000",
                 path,
                 q,
                 i,
@@ -111,7 +114,7 @@ fn gen_queries(path: &str, q: &str) -> Vec<Url> {
 }
 
 #[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
-pub struct Root {
+pub struct CategoryRoot {
     pub realtime_start: String,
     pub realtime_end: String,
     pub order_by: String,
@@ -122,7 +125,7 @@ pub struct Root {
     pub seriess: Vec<Series>,
 }
 
-impl Root {
+impl CategoryRoot {
     fn to_recs(&self) -> Vec<Vec<String>> {
         return self.seriess.iter().map(|x| x.to_rec()).collect::<Vec<_>>();
     }
@@ -166,6 +169,60 @@ impl Series {
             self.notes.to_string(), // fix to just grab source code
         ];
     }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+pub struct SeriesObsRoot {
+    pub realtime_start: String,
+    pub realtime_end: String,
+    pub observation_start: String,
+    pub observation_end: String,
+    pub units: String,
+    pub output_type: i64,
+    pub file_type: String,
+    pub order_by: String,
+    pub sort_order: String,
+    pub count: i64,
+    pub offset: i64,
+    pub limit: i64,
+    pub observations: Vec<Observation>,
+}
+
+impl SeriesObsRoot{
+    fn to_recs(&self) -> Vec<Vec<String>> {
+        return self.observations.iter().map(|x| x.to_rec()).collect::<Vec<_>>();
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+pub struct Observation {
+    pub realtime_start: String,
+    pub realtime_end: String,
+    pub date: String,
+    pub value: String,
+}
+
+impl Observation {
+    fn to_rec(&self) -> Vec<String> {
+        return vec![
+            self.date.to_string(),
+            self.value.to_string()
+        ];
+    }
+}
+
+pub const OBS_HEADER: [&'static str; 2] = [
+    "date", 
+    "value"
+];
+
+
+pub fn read_tickers(filename: impl AsRef<Path>) -> Vec<String> {
+    let f = File::open(filename).expect("no such file");
+    let buf = BufReader::new(f);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
 }
 
 pub const SERIES_HEADER: [&'static str; 13] = [
