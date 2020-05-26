b0a69ffe0c74251e2f4cb653e8f89727e01332af
anandijain <anandj@uchicago.edu>
2020-05-26 10:40:40 -0500

    yf uses fetch now too

--- finox/src/bin/main.rs
+++ finox/src/bin/main.rs
@@ -12,13 +12,15 @@ use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //let tickers = roses::read_tickers("../ref_data/tickers.txt");
-    let symbs: Vec<&str> = finox::headers::CURRENCY_SYMBOLS_YF.to_vec(); //.into_iter().cloned().collect();
-    let urls = gen_yf_urls(symbs);
+    let tickers = roses::read_tickers("../ref_data/tickers_stocks.txt");
+    //let symbs: Vec<&str> = finox::headers::CURRENCY_SYMBOLS_YF.to_vec(); //.into_iter().cloned().collect();
+    //let t = epoch_str(); 
+    //let urls = gen_yfx_urls(symbs);
+    let urls = tickers.iter().map(|x| format!("https://query2.finance.yahoo.com/v8/finance/chart/{}?interval=1d&period1=0&period2=999999999", x)).collect::<Vec<_>>();
     if let Ok(recs) = finox::fetch::<finox::yf::YFRoot>(urls).await {
         println!("{:#?}", recs);
         let file_name = format!(
-            "../data/news/guardian_{}.csv",
+            "../data/yf/yf_{}.csv",
             chrono::Utc::now().to_rfc3339()
         );
         let file_path = Path::new(&file_name);
@@ -26,8 +28,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     Ok(())
 }
+
 // TODO look into the correct pattern for interlacing like this
-pub fn gen_yf_urls(symbs: Vec<&str>) -> Vec<String> {
+pub fn gen_yfx_urls(symbs: Vec<&str>) -> Vec<String> {
     let len = symbs.len();
     let mut urls: Vec<String> = vec![];
     for (i, x) in symbs.iter().enumerate() {
