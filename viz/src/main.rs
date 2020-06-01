e22cdc5b6ffa8045c4ee80c1ece07c4536474b08
anandijain <anandj@uchicago.edu>
2020-06-01 17:38:36 -0500

    added candle to viz, still v jank

--- viz/src/main.rs
+++ viz/src/main.rs
@@ -1,10 +1,10 @@
 extern crate plotters;
+use chrono::DateTime;
 use plotters::prelude::*;
 use serde::Deserialize;
 use std::env;
-
 #[derive(Debug, PartialOrd, PartialEq, Clone, Default, Deserialize)]
-struct Row {
+struct YfRow {
     symb: String,
     t: f32,
     o: f32,
@@ -13,6 +13,15 @@ struct Row {
     c: f32,
     v: u64,
 }
+
+#[derive(Debug, PartialOrd, PartialEq, Clone, Default, Deserialize)]
+struct RtRow {
+    symbol: String,
+    t: String,
+    x: f32,
+    v: u64,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = env::args().collect::<Vec<String>>();
     if args.len() != 3 {
@@ -20,8 +29,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     let input_fold = args[1].clone();
     let input_fn = args[2].clone();
-    let data = get_data(&input_fold, &input_fn);
-    let closes = data.iter().map(|x| x.c).collect::<Vec<f32>>();
+    //let data: Vec<YfRow> = get_data(&input_fold, &input_fn);
+    let data: Vec<RtRow> = get_data(&input_fold, &input_fn);
+    let xs = data.iter().map(|x| x.x).collect::<Vec<f32>>();
+    let ts = data
+        .iter()
+        .map(|x| DateTime::parse_from_rfc3339(&x.t).unwrap().timestamp())
+        .collect::<Vec<i64>>();
+    //let closes = data.iter().map(|x| x.c).collect::<Vec<f32>>();
     //let min =
     let plot_fn = format!("../data/viz/{}.png", input_fn);
     let root = BitMapBackend::new(&plot_fn, (640, 480)).into_drawing_area();
@@ -32,16 +47,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .x_label_area_size(30)
         .y_label_area_size(30)
         .build_ranged(
-            data[0].t..data[data.len() - 1].t,
-            closes[0]..closes[closes.len() - 1],
+            ts[0]..ts[data.len() - 1],
+            //closes[0]..closes[closes.len() - 1],
+            xs[0]..xs[xs.len() - 1],
         )?;
 
-    chart.configure_mesh().draw()?;
+    let to_plot = data
+        .iter()
+        .map(|x| (DateTime::parse_from_rfc3339(&x.t).unwrap().timestamp(), x.x))
+        .into_iter();
 
-    chart
-        .draw_series(LineSeries::new(data.into_iter().map(|x| (x.t, x.c)), &RED))?
-        .label("y = x^2")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    chart.configure_mesh().draw()?;
+    let series = LineSeries::new(to_plot, &RED);
+    chart.draw_series(series);
+
+    //chart
+    //    .draw_series(data.iter().map(|x| {
+    //        CandleStick::new(
+    //            //DateTime::parse_from_rfc3339(x.t).unwrap_or(panic!("fuck dates")),
+    //            x.t, x.o, x.h, x.l, x.c, &GREEN, &RED, 15,
+    //        )
+    //    }))?
+    //    .label("y = x^2")
+    //    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
 
     chart
         .configure_series_labels()
@@ -52,14 +80,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn get_data(folder: &str, file_name: &str) -> Vec<Row> {
+pub fn get_data<'a, T: ?Sized>(folder: &str, file_name: &str) -> Vec<T>
+where
+    for<'de> T: serde::Deserialize<'de> + 'a,
+{
     let file_name_fmtd = format!("../data/{}/{}", folder, file_name);
     let mut rdr = csv::Reader::from_path(file_name_fmtd.clone()).expect(&file_name_fmtd);
     let iter = rdr.deserialize();
     let mut recs = vec![];
     for res in iter {
         if let Ok(r) = res {
-            let rec: Row = r;
+            let rec: T = r;
             recs.push(rec);
         }
     }
