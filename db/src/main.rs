53caeb34f5dfa90028ad9aae15fd261e89de1b79
anandijain <anandj@uchicago.edu>
2020-06-03 18:29:59 -0500

    jank but semi working rt on noria

--- db/src/main.rs
+++ db/src/main.rs
@@ -1,68 +1,71 @@
 use noria::ControllerHandle;
 
-use finox::nasdaq::info::InfoRoot;
-use std::error::Error;
-
+use finox::nasdaq::realtime::RealtimeRoot;
+use std::{error::Error, time::Duration};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut db = ControllerHandle::from_zk("127.0.0.1:2181/yodel3")
+    let mut db = ControllerHandle::from_zk("127.0.0.1:2181/isit4")
         .await
         .unwrap();
 
-    //println!("{:?}", db);
-    //db.install_recipe(
-    //    "
-    //CREATE TABLE Quote(sid varchar(16), t varchar(32),  x varchar(16), PRIMARY KEY(sid));
-    //CREATE TABLE Count(sid varchar(16), qid int);",
-    //)
-    //.await
-    //.unwrap();
-
-    let mut quotes = db.table("Quote").await.unwrap();
-    let mut count = db.table("Count").await.unwrap();
-
-    let (tickers, _) = finox::gen_secs("stocks");
-    let urls = tickers[0..50]
-        .iter()
-        .map(|x| x.to_nasdaq_url("info"))
-        .collect::<Vec<_>>();
-    println!("{:#?}", urls);
-    let recs = finox::fetch_one::<InfoRoot>(urls).await;
-    println!("recs: {:#?}", recs);
-
-    for (i, r) in recs.iter().enumerate() {
-        match r {
-            Some(row) => {
-                quotes
-                    .insert(vec![
-                        row.data.symbol.to_string().into(),
-                        row.data.primary_data.last_sale_price.clone().into(),
-                        row.data.primary_data.last_trade_timestamp.clone().into(),
-                    ])
-                    .await
-                    .unwrap();
-                count
-                    .insert(vec![row.data.symbol.clone().into(), i.into()])
-                    .await
-                    .unwrap();
-            }
-            _ => println!("missing rec"),
-        }
-    }
-
-    db.extend_recipe(
+    db.install_recipe(
         "
-    QuoteCount: \
-      SELECT Count.sid, COUNT(qid) AS counts \
-      FROM Count GROUP BY Count.sid;
-    QUERY ArticleWithVoteCount: \
-      SELECT Quote.sid, t, x, VoteCount.counts AS counts\
-      FROM Quote LEFT JOIN QuoteCount ON (Quote.sid = QuoteCount.sid) \
-      WHERE Quote.sid = ?;",
+    CREATE TABLE Rt(sid varchar(16), t varchar(32), x varchar(16), v varchar(16), PRIMARY KEY(sid));
+    CREATE TABLE Count(sid varchar(16), qid int);",
     )
     .await
     .unwrap();
-    let mut awvc = db.view("ArticleWithVoteCount").await.unwrap();
-    println!("{:#?}", awvc.lookup(&["AAPL".into()], true).await);
+
+    let mut quotes = db.table("Rt").await.unwrap();
+    let mut count = db.table("Count").await.unwrap();
+
+    let (tickers, _) = finox::gen_secs("stocks");
+    let urls = tickers[1..50]
+        .iter()
+        .map(|x| x.to_nasdaq_rt_url())
+        .collect::<Vec<_>>()
+        .into_iter()
+        .flatten()
+        .collect();
+
+    //println!("{:#?}", urls);
+    let recs = finox::fetch::<RealtimeRoot>(urls).await;
+    println!("recs: {:#?}", recs);
+
+    for (_, rs) in recs.iter().enumerate() {
+        for (_, r) in rs.iter().enumerate() {
+            quotes
+                .insert(vec![
+                    r[0].clone().into(),
+                    r[1].clone().into(),
+                    r[2].clone().into(),
+                    r[3].clone().into(),
+                ]) //.iter().map(|x| x.into()).collect::<Vec<_>>())
+                .await
+                .unwrap();
+        }
+    }
+    println!("Finished writing! Let's wait for things to propagate...");
+    tokio::time::delay_for(Duration::from_millis(2000)).await;
+
+    db.extend_recipe(
+    "
+    QUERY Quotes: \
+        SELECT Rt.sid, t, x, v from Rt;
+    "
+    //    "
+    //QuoteCount: \
+    //  SELECT Count.sid, COUNT(qid) AS counts \
+    //  FROM Count GROUP BY Count.sid;
+    //QUERY ArticleWithQuoteCount: \
+    //  SELECT Quote.sid, t, x, QuoteCount.counts AS counts\
+    //  FROM Quote LEFT JOIN QuoteCount ON (Quote.sid = QuoteCount.sid) \
+    //  WHERE Quote.sid = ?;",
+    )
+    .await
+    .unwrap();
+    let mut awvc = db.view("Quotes").await.unwrap();
+    let article = awvc.lookup(&["aapl".into()], true).await.unwrap();
+    println!("{:#?} {:#?} ", awvc.len().await, article);
     Ok(())
 }
