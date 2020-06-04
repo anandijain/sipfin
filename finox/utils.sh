7d4961ec727587c8dd35b6216c5c4dd1776f5d30
anandijain <anandj@uchicago.edu>
2020-06-04 13:29:23 -0500

    simple curl script for sec links

--- finox/utils.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-options() {
-    curl "ftp://ftp.nasdaqtrader.com/symboldirectory/options.txt" -o "./ref_data/options.txt"
-}
-
---
+++ utils.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+options() {
+    curl "ftp://ftp.nasdaqtrader.com/symboldirectory/options.txt" -o "./ref_data/options.txt"
+}
+
+for YEAR in {1993..2020}
+do 
+	for QTR in {1..4}
+	do 
+		URL="https://www.sec.gov/Archives/edgar/full-index/$YEAR/QTR$QTR/master.idx" 
+		OPATH="./ref_data/sec/master_$YEAR$QTR.idx"
+		echo "$URL, to $OPATH"
+		curl $URL -o $OPATH
+	done
+done
+
+
+for f in ./ref_data/sec/*.idx
+do 
+	sed -i -e 1,9d $f
+	sed -i -e 2d $f
+done
+
